3c722f3778c2bd8625e3da99eb37524db2f534c2
Edson Camacho <edsonlpcamacho@gmail.com>
2024-10-26 07:42:15 -0300

    Database connection completed

--- src/main/java/com/gourmetx/GourmetX/controllers/UserController.java
+++ src/main/java/com/gourmetx/GourmetX/controllers/UserController.java
@@ -1,9 +1,13 @@
 package com.gourmetx.GourmetX.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gourmetx.GourmetX.entities.User;
@@ -18,5 +22,17 @@ public class UserController {
     @GetMapping("/users")
     public List<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<?> loginUser(@RequestBody User loginRequest) {
+        Optional<User> user = userRepository.findByUsernameAndPassword(loginRequest.getUsername(), loginRequest.getPassword());
+        
+        if (user.isPresent()) {
+            // Aqui você pode gerar e retornar um token, ou os dados do usuário, conforme necessário.
+            return ResponseEntity.ok(user.get());
+        } else {
+            return ResponseEntity.status(401).body("Credenciais inválidas");
+        }
     }
 }
--- src/main/java/com/gourmetx/GourmetX/repositories/UserRepository.java
+++ src/main/java/com/gourmetx/GourmetX/repositories/UserRepository.java
@@ -1,9 +1,14 @@
 package com.gourmetx.GourmetX.repositories;
 
-
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import com.gourmetx.GourmetX.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    // Método usando consulta personalizada
+    @Query("SELECT u FROM User u WHERE u.username = :username AND u.password = :password")
+    Optional<User> findByUsernameAndPassword(@Param("username") String username, @Param("password") String password);
 }
